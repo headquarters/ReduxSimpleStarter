8413277560d70a6becacce4f489d25da8286d3f1
Michael Head <michael.head@marketpay.com>
2017-01-26 10:08:25 -0500

    Added resources route

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Super Special Recipe
+            <ul>
+                <li>3 eggs</li>
+                <li>2 cups of flour</li>
+                <li>1 cup of water</li>
+            </ul>
+        </div>
+    );
+};
