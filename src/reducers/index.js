7d7e89e6e15a45ba5c6c8837c49c953863f856fb
Michael Head <michaelehead@gmail.com>
2017-01-29 11:08:20 -0500

    Finished HOC section

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import authenticationReducer from './authenication';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  authenticated: authenticationReducer
 });
 
 export default rootReducer;
