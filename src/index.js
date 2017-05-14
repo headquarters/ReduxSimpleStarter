bdc42d8e9c5149c02c658093deaf192c1c9d8fc7
Michael Head <michaelehead@gmail.com>
2017-05-14 11:12:08 -0400

    Handle refreshing page; check token existence

--- src/index.js
+++ src/index.js
@@ -6,23 +6,33 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
-import requireAuth from './components/auth/require-auth';
+import Home from './components/home';
+import RequireAuth from './components/auth/require-auth';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
 import Feature from './components/feature';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+
+if (token) {
+  // if there is a token we assume the user is signed in
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
         <Route path="signin" component={Signin}/>
         <Route path="signout" component={Signout}/>
         <Route path="signup" component={Signup}/>
-        <Route path="feature" component={requireAuth(Feature)}/>
+        <Route path="feature" component={RequireAuth(Feature)}/>
       </Route>
     </Router>
   </Provider>
