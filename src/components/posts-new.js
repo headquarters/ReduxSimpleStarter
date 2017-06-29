de3770c4d61f2793840fb1471c7248457c946787
Michael Head <michael.head@marketpay.com>
2017-06-29 17:00:13 -0400

    Validation working

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -2,43 +2,73 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
-    return <input type="text" />;
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label htmlFor={field.id}>{field.label}</label>
+        <input 
+          className="form-control"
+          type="text"
+          id={field.id}
+          {...field.input} />
+          {field.meta.touched && field.meta.error}
+      </div>
+    )
+  }
+
+  onSubmit(values) {
+    console.log(values);
   }
 
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <form>
-        <div className="form-group">
-          <label htmlFor="title">Title</label>
-          <Field
-            name="title"
-            className="form-control"
-            component="input"
-            type="text" 
-            id="title"
-            />
-            <label htmlFor="tags">Tags</label>
-            <Field
-              name="tags"
-              className="form-control"
-              component="input"
-              type="text" 
-              id="tags"
-              />
-            <label htmlFor="content">Content</label>
-            <Field
-              name="content"
-              className="form-control"
-              component="textarea"
-              id="content"
-              />
-          </div>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          name="title"
+          label="Title"
+          id="title"
+          component={this.renderField}
+          />
+        <Field
+          name="categories"
+          label="Categories"
+          component={this.renderField}
+          id="categories"
+          />
+        <Field
+          name="content"
+          label="Content"
+          component={this.renderField}
+          id="content"
+          />
+          <button type="submit">Submit</button>
       </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+
+  // if errors is empty, form will submit
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
